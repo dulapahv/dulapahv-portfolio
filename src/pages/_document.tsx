2c97acb21802cea639ae8fa1d23df157e893fdd9
Dulapah Vibulsanti <dulapah6@gmail.com>
2023-11-06 10:54:33 +0000

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,8 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
-      {/* Google Analytics */}
-      <Script src='https://www.googletagmanager.com/gtag/js?id=G-Q1V5DFC2B7' />
-      <Script id='google-analytics'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-Q1V5DFC2B7');
-        `}
-      </Script>
-
       {/* Standard Meta Tags */}
       <meta charSet='UTF-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Script from 'next/script';
+
 import Skill from './skill';
 import Header from './header';
 import AboutMe from './aboutme';
@@ -10,6 +12,17 @@ import Experience from './experience';
 const Index = () => {
   return (
     <>
+      {/* Google Analytics */}
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-Q1V5DFC2B7' />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Q1V5DFC2B7');
+        `}
+      </Script>
       <div
         className='fixed w-[15rem] sm:w-[20rem] md:w-[30rem] lg:w-[40rem] [background-size:105%] sm:[background-size:79%] md:[background-size:53%] lg:[background-size:40%] bg-repeat dark:bg-black/40 min-h-screen overflow-hidden dark:brightness-[.33] z-0'
         style={{
