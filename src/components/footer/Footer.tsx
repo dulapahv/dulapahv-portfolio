49f00518826b7e4df088c979ca0697584d321ce2
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-03-21 01:26:26 +0000

    refactor: update time

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Last updated: 29/01/2023 - 19.00 GMT
+          Last updated: 21/03/2023 - 01.00 GMT
         </a>
       </time>
     </footer>
