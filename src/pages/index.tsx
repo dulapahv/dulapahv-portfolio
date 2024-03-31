47e4cce34113a8d5848b8f18c6adf5af65cff714
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-03-31 22:38:28 +0100

    refactor: fixed max width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ const Index = () => {
   const projectRef = useRef<HTMLDivElement>(null);
 
   return (
-    <>
-      <div className="bg_pattern fixed z-0 min-h-screen w-[15rem] overflow-hidden bg-repeat [background-size:105%] dark:bg-black/40 dark:brightness-[.33] sm:w-[20rem] sm:[background-size:79%] md:w-[30rem] md:[background-size:53%] lg:w-[40rem] lg:[background-size:40%]" />
+    <div className="flex flex-col items-center [&>*:not(:first-child)]:max-w-[3840px]">
+      <div className="bg_pattern fixed left-0 z-0 min-h-screen w-[15rem] overflow-hidden bg-repeat [background-size:105%] dark:bg-black/40 dark:brightness-[.33] sm:w-[20rem] sm:[background-size:79%] md:w-[30rem] md:[background-size:53%] lg:w-[40rem] lg:[background-size:40%]" />
       <Header sectionRef={aboutmeRef} />
       <AboutMe ref={aboutmeRef} />
       <Education ref={educationRef} />
@@ -36,7 +36,7 @@ const Index = () => {
           project: projectRef,
         }}
       />
-    </>
+    </div>
   );
 };
 
