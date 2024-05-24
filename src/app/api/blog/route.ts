d35fa6957729c02489260c09a3582f06221b4532
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-05-24 10:53:03 +0100

    fix: build error

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,1 +1,16 @@
-export async function GET(request: Request) {}
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/db";
+
+export async function GET(request: Request) {
+  const response = await prisma.blog.findMany();
+
+  return NextResponse.json(
+    {
+      response,
+    },
+    {
+      status: 200,
+    },
+  );
+}
