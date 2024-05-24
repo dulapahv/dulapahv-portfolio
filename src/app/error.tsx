7294f4c02b42451122de0402ce2b577b2165b4ed
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-05-24 12:13:20 +0100

    debug: test error handling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import React from "react";
+import Error from "next/error";
+import Sentry from "@sentry/nextjs";
 
-const Error = () => {
+const ErrorPage = (props: any) => {
   return (
     <div>
       <div>Error</div>
+      <Error {...props} />
     </div>
   );
 };
 
-export default Error;
+export default ErrorPage;
+
+ErrorPage.getInitialProps = async (contextData: any) => {
+  // In case this is running in a serverless function, await this in order to give Sentry
+  // time to send the error before the lambda exits
+  await Sentry.captureUnderscoreErrorException(contextData);
+
+  // This will contain the status code of the response
+  return Error.getInitialProps(contextData);
+};
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Blog = () => {
+  const simulateError = () => {
+    throw new Error("Simulated error");
+  };
   return (
     <div>
       <div>Blog</div>
+      <button onClick={simulateError}>
+        A
+      </button>
     </div>
   );
 };
