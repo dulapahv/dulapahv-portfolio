0f3de6e480c062923c10721c8067b6100672d130
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-06-10 14:16:46 +0100

    Update dynamicBlurDataUrl.ts

--- src/utils/dynamicBlurDataUrl.ts
+++ src/utils/dynamicBlurDataUrl.ts
@@ -1,7 +1,9 @@
 const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/"
-    : process.env.NEXT_PUBLIC_VERCEL_URL;
+    : process.env.VERCEL_URL;
+
+console.log("baseUrl", baseUrl);
 
 const dynamicBlurDataUrl = async (url: string) => {
   const base64str = await fetch(
