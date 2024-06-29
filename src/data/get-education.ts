e470fc8c4ceb713ece5ce73449989e605539467e
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-06-29 19:47:57 +0100

    feat: use Prisma accelerate for caching

--- src/data/get-education.ts
+++ src/data/get-education.ts
@@ -3,12 +3,30 @@ import { cache } from "react";
 import prisma from "@/lib/prisma";
 
 export const getManyEducation = cache(async (query?: any) => {
-  const item = await prisma.education.findMany(query);
-  const count = await prisma.education.count({ where: query.where });
+  const item = await prisma.education.findMany({
+    ...query,
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
+  });
+  const count = await prisma.education.count({
+    where: query.where,
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
+  });
   return { item, count };
 });
 
 export const getUniqueEducation = cache(async (query?: any) => {
-  const item = await prisma.education.findUnique(query);
+  const item = await prisma.education.findUnique({
+    ...query,
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
+  });
   return item;
 });
--- src/data/get-project.ts
+++ src/data/get-project.ts
@@ -3,12 +3,30 @@ import { cache } from "react";
 import prisma from "@/lib/prisma";
 
 export const getManyProject = cache(async (query?: any) => {
-  const item = await prisma.project.findMany(query);
-  const count = await prisma.project.count({ where: query.where });
+  const item = await prisma.project.findMany({
+    ...query,
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
+  });
+  const count = await prisma.project.count({
+    where: query.where,
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
+  });
   return { item, count };
 });
 
 export const getUniqueProject = cache(async (query: any) => {
-  const item = await prisma.project.findUnique(query);
+  const item = await prisma.project.findUnique({
+    ...query,
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
+  });
   return item;
 });
