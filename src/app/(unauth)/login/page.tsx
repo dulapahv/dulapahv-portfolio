458c0027531dd7b9cdfc41b69d704e240043c45b
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-07-07 12:03:11 +0100

    debug: add mdxeditor

--- src/app/(unauth)/login/page.tsx
+++ src/app/(unauth)/login/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import React from "react";
+
+import { Editor } from "@/ui/mdx-editor";
 
 export default function Page() {
   return (
     <div>
       <div>Login</div>
+      <div className=''>
+        <Editor
+          onChange={(str) => {
+            console.log(str);
+          }}
+          markdown=""
+          markdownOld=""
+        />
+      </div>
     </div>
   );
 }
