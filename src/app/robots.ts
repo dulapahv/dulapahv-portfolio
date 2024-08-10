c91b7fe7a8e91a460745a2e95c7e54c84738ba79
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-08-10 10:42:10 +0100

    refactor: disallow crawling of some images

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/blue.png", "/pink.png", "/fallback.png"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
     host: BASE_URL,
