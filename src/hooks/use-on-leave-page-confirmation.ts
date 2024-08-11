3b7e3790383e7a2331deaea4b1042b920427c02f
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-08-11 08:52:20 +0100

    fix: un page confirmation not working in prod

--- src/hooks/use-on-leave-page-confirmation.ts
+++ src/hooks/use-on-leave-page-confirmation.ts
@@ -1,18 +1,37 @@
-"use client";
-
 import { useEffect } from "react";
+import { NavigateOptions } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useRouter } from "next/navigation";
 
-export function useOnLeavePageConfirmation(isDirty: boolean) {
+export function useOnLeavePageConfirmation(hasUnsavedChanges: boolean) {
+  const router = useRouter();
+
   useEffect(() => {
-    function beforeUnload(e: BeforeUnloadEvent) {
-      if (!isDirty) return;
-      e.preventDefault();
-    }
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (hasUnsavedChanges) {
+        event.preventDefault();
+      }
+    };
 
-    window.addEventListener("beforeunload", beforeUnload);
+    const originalPush = router.push;
+
+    router.push = (url: string, options?: NavigateOptions) => {
+      if (hasUnsavedChanges) {
+        const confirmLeave = window.confirm(
+          "Changes you made may not be saved.",
+        );
+        if (confirmLeave) {
+          originalPush(url, options);
+        }
+      } else {
+        originalPush(url, options);
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => {
-      window.removeEventListener("beforeunload", beforeUnload);
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      router.push = originalPush;
     };
-  }, [isDirty]);
+  }, [router, hasUnsavedChanges]);
 }
