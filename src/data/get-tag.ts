c30dc50020e9b5484dc7017bf0b322a6f3c6d9b9
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-10-22 11:55:42 +0100

    fix: revert cache removal

--- src/data/get-tag.ts
+++ src/data/get-tag.ts
@@ -5,11 +5,17 @@ import prisma from "@/lib/prisma";
 export const getManyTag = cache(async (query?: any) => {
   const item = await prisma.tag.findMany({
     ...query,
-
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
   });
   const count = await prisma.tag.count({
     where: query.where,
-
+    cacheStrategy: {
+      ttl: 2_592_000, // 1 month
+      swr: 7_776_000, // 3 month
+    },
   });
   return { item, count };
 });
