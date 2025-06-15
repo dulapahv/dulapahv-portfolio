788a6cf669cb1554a33dffc11803da388a567f7b
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-06-16 00:54:18 +0100

    refactor: remove unused code

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -8,7 +8,6 @@ import { cn } from '@/lib/utils';
 
 import { Link } from './link';
 import Pre from './pre';
-import { TableOfContents } from './toc';
 import { Video } from './video';
 
 type MdxProperties = {
@@ -78,68 +77,60 @@ const createHeading = (level: 'h2' | 'h3' | 'h4' | 'h5' | 'h6') => {
 };
 
 export const Mdx = ({ code }: MdxProperties) => (
-  <>
-    {/* <div className="hidden xl:block">
-      <TableOfContents />
-    </div> */}
-    <article
-      aria-label="Article content"
-      className={cn(
-        'mx-auto max-w-none font-medium',
-        'prose prose-neutral dark:prose-invert',
-        `prose-headings:mt-8 prose-headings:font-semibold
-        prose-headings:[&>a]:text-foreground`,
-        'prose-h1:text-3xl prose-h1:font-bold',
-        `prose-h2:text-2xl prose-h2:mb-6 prose-h2:mt-12 prose-h2:border-t
-        prose-h2:border-border prose-h2:pt-10`,
-        'prose-h3:text-xl prose-h3:mt-8 prose-h3:mb-3',
-        'prose-h4:text-lg prose-h4:mt-6 prose-h4:mb-2',
-        'prose-h5:text-base prose-h5:mt-4 prose-h5:mb-1',
-        'prose-h6:text-sm prose-h6:mt-2 prose-h6:mb-0',
-        'prose-p:my-5',
-        'prose-a:no-underline',
-        `prose-pre:dark:bg-[#0a0a0a] prose-pre:bg-[#fff] prose-pre:border-t-border
-        prose-pre:text-[13px]/5`,
-        `prose-figure:border prose-figure:border-border prose-figure:rounded-md
-        prose-figure:my-4 prose-figure:relative
-        prose-figure:[figure:has(figcaption)&>pre]:rounded-t-none
-        prose-figure:[figure:has(figcaption)&>pre]:border-t`,
-        `prose-figcaption:dark:bg-[#000] prose-figcaption:bg-[#fafafa]
-        prose-figcaption:mt-0 prose-figcaption:py-2.5 prose-figcaption:rounded-t-md
-        prose-figcaption:px-4`,
-        'prose-figure:[figure:has(figcaption)&>button]:top-1',
-        `prose-blockquote:not-italic prose-blockquote:[&>p]:before:content-none
-        prose-blockquote:[&>p]:after:content-none prose-blockquote:border
-        prose-blockquote:border-border prose-blockquote:[&>p]:my-0
-        prose-blockquote:rounded-md prose-blockquote:p-3 prose-blockquote:text-sm`,
-        `prose-code:before:content-none prose-code:after:content-none
-        prose-code:dark:bg-[#0a0a0a] prose-code:bg-[#fff]
-        prose-code:[&:not([data-theme])]:px-1 prose-code:[&:not([data-theme])]:py-px
-        prose-code:rounded-sm prose-code:[&:not([data-theme])]:border
-        prose-code:[&:not([data-theme])]:border-border
-        prose-code:[&:not([data-theme])]:font-normal
-        prose-code:[&:not([data-theme])]:text-[13px] prose-code:text-foreground`,
-        `[&>details>summary]:cursor-pointer [&>details>summary]:hover:underline
-        [&>details>ul]:my-0`,
-      )}
-    >
-      {/* <div className="xl:hidden">
-        <TableOfContents />
-      </div> */}
-      <MDXContent
-        code={code}
-        components={{
-          a,
-          pre: Pre,
-          img,
-          Video,
-          h2: createHeading('h2'),
-          h3: createHeading('h3'),
-          h4: createHeading('h4'),
-          h5: createHeading('h5'),
-          h6: createHeading('h6'),
-        }}
-      />
-    </article>
-  </>
+  <article
+    aria-label="Article content"
+    className={cn(
+      'mx-auto max-w-none font-medium',
+      'prose prose-neutral dark:prose-invert',
+      `prose-headings:mt-8 prose-headings:font-semibold
+      prose-headings:[&>a]:text-foreground`,
+      'prose-h1:text-3xl prose-h1:font-bold',
+      `prose-h2:text-2xl prose-h2:mb-6 prose-h2:mt-12 prose-h2:border-t
+      prose-h2:border-border prose-h2:pt-10`,
+      'prose-h3:text-xl prose-h3:mt-8 prose-h3:mb-3',
+      'prose-h4:text-lg prose-h4:mt-6 prose-h4:mb-2',
+      'prose-h5:text-base prose-h5:mt-4 prose-h5:mb-1',
+      'prose-h6:text-sm prose-h6:mt-2 prose-h6:mb-0',
+      'prose-p:my-5',
+      'prose-a:no-underline',
+      `prose-pre:dark:bg-[#0a0a0a] prose-pre:bg-[#fff] prose-pre:border-t-border
+      prose-pre:text-[13px]/5`,
+      `prose-figure:border prose-figure:border-border prose-figure:rounded-md
+      prose-figure:my-4 prose-figure:relative
+      prose-figure:[figure:has(figcaption)&>pre]:rounded-t-none
+      prose-figure:[figure:has(figcaption)&>pre]:border-t`,
+      `prose-figcaption:dark:bg-[#000] prose-figcaption:bg-[#fafafa]
+      prose-figcaption:mt-0 prose-figcaption:py-2.5 prose-figcaption:rounded-t-md
+      prose-figcaption:px-4`,
+      'prose-figure:[figure:has(figcaption)&>button]:top-1',
+      `prose-blockquote:not-italic prose-blockquote:[&>p]:before:content-none
+      prose-blockquote:[&>p]:after:content-none prose-blockquote:border
+      prose-blockquote:border-border prose-blockquote:[&>p]:my-0
+      prose-blockquote:rounded-md prose-blockquote:p-3 prose-blockquote:text-sm`,
+      `prose-code:before:content-none prose-code:after:content-none
+      prose-code:dark:bg-[#0a0a0a] prose-code:bg-[#fff]
+      prose-code:[&:not([data-theme])]:px-1 prose-code:[&:not([data-theme])]:py-px
+      prose-code:rounded-sm prose-code:[&:not([data-theme])]:border
+      prose-code:[&:not([data-theme])]:border-border
+      prose-code:[&:not([data-theme])]:font-normal
+      prose-code:[&:not([data-theme])]:text-[13px] prose-code:text-foreground`,
+      `[&>details>summary]:cursor-pointer [&>details>summary]:hover:underline
+      [&>details>ul]:my-0`,
+    )}
+  >
+    <MDXContent
+      code={code}
+      components={{
+        a,
+        pre: Pre,
+        img,
+        Video,
+        h2: createHeading('h2'),
+        h3: createHeading('h3'),
+        h4: createHeading('h4'),
+        h5: createHeading('h5'),
+        h6: createHeading('h6'),
+      }}
+    />
+  </article>
 );
