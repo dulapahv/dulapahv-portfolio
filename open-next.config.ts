334a98177271e27420e441fea3d871882d6ac93b
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-10-05 21:04:59 +0100

    fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,9 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import staticAssetsIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
 
 export default defineCloudflareConfig({
-  incrementalCache: staticAssetsIncrementalCache,
+  incrementalCache: r2IncrementalCache,
+  queue: doQueue,
+  enableCacheInterception: true,
 });
