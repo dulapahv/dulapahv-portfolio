5811c19cddc54c3913f6548ca4b4b9def93988f9
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-10-06 13:01:11 +0100

    refactor: move redirects to DNS level

--- next.config.ts
+++ next.config.ts
@@ -16,28 +16,8 @@ const nextConfig: NextConfig = {
     typedEnv: true,
     viewTransition: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/resume',
-        destination:
-          'https://1drv.ms/b/c/bac8961c289dd3e4/EboeX5e_xXZNrQKQv5skNkMBjebYxatKHkIFgHb2-0-3pA',
-        permanent: true,
-      },
-      {
-        source: '/internship(.pdf)?',
-        destination: 'https://assets.dulapahv.dev/internship.pdf',
-        permanent: true,
-      },
-      {
-        source: '/admin',
-        destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        permanent: false,
-      },
-    ];
-  },
 };
 
-export default withContentCollections(nextConfig);
-
 initOpenNextCloudflareForDev();
+
+export default withContentCollections(nextConfig);
