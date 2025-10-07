c3eb40f33a3b4a93ba850f8c9d6d3834f8dfefe9
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-10-08 00:12:15 +0100

    fix: remove unsupported regional cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,9 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
-import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(kvIncrementalCache, {
-    mode: 'long-lived',
-    shouldLazilyUpdateOnCacheHit: true,
-  }),
+  incrementalCache: kvIncrementalCache,
   queue: doQueue,
   enableCacheInterception: true,
 });
