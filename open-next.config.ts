f1891bf21d478ae8af2e2d4c1fe9d5755ec51302
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-10-08 00:09:21 +0100

    feat: move from r2 to kv

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,10 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
 import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
+  incrementalCache: withRegionalCache(kvIncrementalCache, {
     mode: 'long-lived',
     shouldLazilyUpdateOnCacheHit: true,
   }),
