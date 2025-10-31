136f10ef3c880e2e938bfc5bfd7b6bc286d91a5a
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-10-31 17:10:54 +0000

    refactor: move back to Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,20 @@
 import type { NextConfig } from 'next';
 
 import { withContentCollections } from '@content-collections/next';
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [process.env.ALLOWED_DEV_ORIGINS || '[]'],
-  // reactCompiler: true,
+  reactCompiler: true,
   poweredByHeader: false,
   typedRoutes: true,
   images: {
-    loader: 'custom',
-    loaderFile: './image-loader.ts',
     qualities: [75, 100]
   },
   experimental: {
     typedEnv: true,
     viewTransition: true,
-    // turbopackFileSystemCacheForDev: true
+    turbopackFileSystemCacheForDev: true
   }
 };
-
-initOpenNextCloudflareForDev();
 
 export default withContentCollections(nextConfig);
