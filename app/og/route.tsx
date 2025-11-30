bd8ad48eb0efb8453eb01b366e4f287259f1420e
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-11-30 11:21:03 +0000

    refactor: prettify code

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -18,28 +18,26 @@ export const GET = async (request: NextRequest) => {
   const geistRegular = await readFile(join(process.cwd(), 'app/og/Geist-Regular.ttf'));
 
   return new ImageResponse(
-    (
-      <div
-        tw="flex flex-col justify-between items-start w-full h-full bg-black p-12"
-        style={{
-          backgroundSize: '80px 80px',
-          backgroundImage:
-            'linear-gradient(to right, #1C1C1C 1px, transparent 1px), linear-gradient(to bottom, #1C1C1C 1px, transparent 1px)'
-        }}
-      >
-        <img src={logoSrc} alt="avatar" width={72} height={72} tw="overflow-hidden" />
-        <div tw="flex flex-col">
-          <h1 tw="max-w-[50rem] text-[64px] font-bold leading-[69px] tracking-tighter m-0 text-[#F1F1F1]">
-            {title}
-          </h1>
-          {description && (
-            <p tw="max-w-[64rem] text-[24px] font-normal leading-[32px] tracking-tight text-[#A5A5A5] mt-4 mb-0">
-              {description}
-            </p>
-          )}
-        </div>
+    <div
+      tw="flex flex-col justify-between items-start w-full h-full bg-black p-12"
+      style={{
+        backgroundSize: '80px 80px',
+        backgroundImage:
+          'linear-gradient(to right, #1C1C1C 1px, transparent 1px), linear-gradient(to bottom, #1C1C1C 1px, transparent 1px)'
+      }}
+    >
+      <img src={logoSrc} alt="avatar" width={72} height={72} tw="overflow-hidden" />
+      <div tw="flex flex-col">
+        <h1 tw="max-w-[50rem] text-[64px] font-bold leading-[69px] tracking-tighter m-0 text-[#F1F1F1]">
+          {title}
+        </h1>
+        {description && (
+          <p tw="max-w-[64rem] text-[24px] font-normal leading-[32px] tracking-tight text-[#A5A5A5] mt-4 mb-0">
+            {description}
+          </p>
+        )}
       </div>
-    ),
+    </div>,
     {
       width: 1200,
       height: 630,
