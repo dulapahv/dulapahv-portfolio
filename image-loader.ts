66a8c8771911962efcb18ce8913de67522320f97
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-12-02 13:01:03 +0000

    fix: revert

---
+++ image-loader.ts
@@ -0,0 +1,20 @@
+import type { ImageLoaderProps } from 'next/image';
+
+const normalizeSrc = (src: string) => {
+  return src.startsWith('/') ? src.slice(1) : src;
+};
+
+export default function cloudflareLoader({ src, width, quality }: ImageLoaderProps) {
+  if (src.startsWith('http://') || src.startsWith('https://')) {
+    return src;
+  }
+
+  const params = [`width=${width}`];
+  if (quality) {
+    params.push(`quality=${quality}`);
+  }
+  if (process.env.NODE_ENV === 'development') {
+    return `${src}?${params.join('&')}`;
+  }
+  return `/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`;
+}
