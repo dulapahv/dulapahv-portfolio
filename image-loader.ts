1fa7e2c74ee8016db6defadcb3e36c024c3b2353
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-12-02 13:51:53 +0000

    fix: nextjs image width warning 2

--- image-loader.ts
+++ image-loader.ts
@@ -9,8 +9,15 @@ export default function cloudflareLoader({ src, width, quality }: ImageLoaderPro
   if (quality) {
     params.push(`quality=${quality}`);
   }
+  // For external URLs
+  if (src.startsWith('http://') || src.startsWith('https://')) {
+    if (process.env.NODE_ENV === 'development') {
+      return `${src}?${params.join('&')}`;
+    }
+    return `/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`;
+  }
+  // For local images
   if (process.env.NODE_ENV === 'development') {
     return `${src}?${params.join('&')}`;
   }
-  return `/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`;
 }
