4a2199ee4cfdd1fc11a81c52c1577f80f65a9ca8
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-12-29 12:41:55 +0000

    refactor: add min height to loading card

--- components/open-source-card/loading.tsx
+++ components/open-source-card/loading.tsx
@@ -1,5 +1,7 @@
 import { Card } from "@/components/card";
 
 export function Loading() {
-  return <Card className="relative overflow-hidden" />;
+  return (
+    <Card className="relative overflow-hidden" containerClassName="min-h-96" />
+  );
 }
