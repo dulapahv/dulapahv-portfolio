89dd3d847cfe97da742be0c4d72e326458ec6b2a
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-02-16 00:46:16 +0000

    refactor: change img quality for slug page

--- app/[type]/page.tsx
+++ app/[type]/page.tsx
@@ -164,7 +164,7 @@ export default async function TypeListingPage({
                                       alt={post.title}
                                       className="object-cover"
                                       fill
-                                      quality={5}
+                                      quality={25}
                                       sizes="150px"
                                       src={post.image}
                                     />
