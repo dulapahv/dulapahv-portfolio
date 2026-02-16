f30214d48075fd7f0a251b6d4841032b6f1d22b1
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-02-16 00:01:15 +0000

    refactor: prevent bot from crawling camera roll img

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/camera_roll/",
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
