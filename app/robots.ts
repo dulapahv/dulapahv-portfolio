dae3345b089879dd98226457f1e994abd9359d51
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-03-21 12:50:09 +0000

    fix: missing robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/camera-roll/",
+      disallow: ["/camera_roll/", "/camera-roll/"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
