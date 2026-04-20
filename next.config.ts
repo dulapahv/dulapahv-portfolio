5bb11e1ad62f833ab561a4815b8f28a24751e26a
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-04-20 09:48:36 +0100

    chore: enable turbopack build cache and server-side async chunking

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,7 @@ const nextConfig: NextConfig = {
     viewTransition: true,
     // inlineCss: true,
     turbopackFileSystemCacheForBuild: true,
+    turbopackServerSideNestedAsyncChunking: true,
     optimizePackageImports: ["@phosphor-icons/react", "motion"],
   },
   images: {
